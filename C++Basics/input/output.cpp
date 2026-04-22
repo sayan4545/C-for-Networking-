a9cd7908920e0ce750ed0eef09ce2e9bdc779068
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-22 14:34:24 +0530

    Taking input

---
+++ C++Basics/input/output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Enter the two values: "<<endl;
+    cin>>x>>y;
+    cout<<"The sum of the two values is : "<<endl;
+    cout<<x+y<<endl;
+}
